09e04a1470049b519beddd48991c189d83198319
Michael Camacho <mikedev0431@gmail.com>
2019-09-18 12:36:01 -0700

    Added private route component import. As well as added PrivateRoute component with a path to dashboard and component dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Dashboard from './components/ui/screens/Dashboard';
 import Welcome from './components/ui/screens/Welcome';
-import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/ui/routes/PrivateRoute';
+
+interface AppState {
+  isAuthenticated: boolean;
+}
 
 const App: React.FC = () => {
+  const [globalValues, setGlobalValues] = useState<AppState>({
+    isAuthenticated: false
+  });
+
   return (
     <React.Fragment>
       <Switch>
         <Route path="/" exact component={Welcome} />
-        <Route path="/register" component={Welcome} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/register" component={Welcome} />=
+        <PrivateRoute
+          path="/dashboard"
+          component={Dashboard}
+          isAuthenticated={globalValues.isAuthenticated}
+        />
       </Switch>
     </React.Fragment>
   );

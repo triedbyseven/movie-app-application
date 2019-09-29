048c4181f2e5a4bff1c145e5392ac00d43d3bc3a
Michael Camacho <mikedev0431@gmail.com>
2019-09-29 16:23:37 -0700

    Added jwt decode module. Destructered userId if there is already a token. Added register method as a action to set auth cookie and user Id

--- src/userActions.js
+++ src/userActions.js
@@ -1,13 +1,16 @@
 import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
 
 // Check if user is already logged in with by checking if there is a auth cookie
 let token = Cookies.get('Authorization') || '';
 token = token.split(' ')[1];
 
+const { id: userId } = token ? jwtDecode(token) : '';
+
 export const userInitialState = {
   user: {
     loggedIn: token != null,
-    userId: '',
+    userId: userId,
     token: token
   }
 };
@@ -30,5 +33,19 @@ export const userActions = {
   logout: state => {
     Cookies.remove('Authorization');
     return { user: { loggedIn: false } };
+  },
+  register: (state, args) => {
+    const token = args.token;
+    const userId = args.id;
+
+    Cookies.set('Authorization', `Bearer ${token}`);
+
+    return {
+      user: {
+        loggedIn: true,
+        userId: userId,
+        token: token
+      }
+    };
   }
 };

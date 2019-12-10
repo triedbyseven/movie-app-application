7aa949824f6c9980d0517d16cac9d5e07d6da474
triedbyseven <mikedev0431@gmail.com>
2019-12-10 12:39:14 -0800

    Moved all schemas into their own objects outside of function. We removed all schemas being called withing he function and added them the bottom of the module. This will help with better understanding what the functions do themselves and is better abstraction.

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,17 +1,7 @@
 import Joi from '@hapi/joi';
 
 async function validateUser(username: string, password: string) {
-  const schema = Joi.object({
-    username: Joi.string()
-      .alphanum()
-      .min(5)
-      .max(20)
-      .required(),
-
-    password: Joi.string().regex(/^[a-zA-Z0-9!@#]{0,30}$/)
-  });
-
-  return await schema.validate(
+  return await validateUserSchema.validate(
     { username: username, password: password },
     { abortEarly: false }
   );
@@ -22,25 +12,35 @@ async function validateRegisteredUser(
   email: string,
   password: string
 ) {
-  const schema = Joi.object({
-    username: Joi.string()
-      .alphanum()
-      .min(5)
-      .max(20)
-      .required(),
-
-    email: Joi.string().email({
-      minDomainSegments: 2,
-      tlds: { allow: ['com', 'net'] }
-    }),
-
-    password: Joi.string().regex(/^[a-zA-Z0-9!@#]{0,30}$/)
-  });
-
-  return await schema.validate(
+  return await validateRegisteredUserSchema.validate(
     { username: username, email: email, password: password },
     { abortEarly: false }
   );
 }
+
+const validateUserSchema = Joi.object({
+  username: Joi.string()
+    .alphanum()
+    .min(5)
+    .max(20)
+    .required(),
+
+  password: Joi.string().regex(/^[a-zA-Z0-9!@#]{0,30}$/)
+});
+
+const validateRegisteredUserSchema = Joi.object({
+  username: Joi.string()
+    .alphanum()
+    .min(5)
+    .max(20)
+    .required(),
+
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] }
+  }),
+
+  password: Joi.string().regex(/^[a-zA-Z0-9!@#]{0,30}$/)
+});
 
 export { validateUser, validateRegisteredUser };

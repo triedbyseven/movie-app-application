6f54dff8ca9e7432116f8009970d1c45d2f69927
triedbyseven <mikedev0431@gmail.com>
2019-12-15 00:13:26 -0800

    Removed axios as a depency and method for movie api service. Moved the getMovies method out from this component and put it in it's own module.
    Removed axios depency out from this component and into it's own module.
    
    Added new import for `getMovies` from it's own service module.

--- src/components/ui/screens/Dashboard.tsx
+++ src/components/ui/screens/Dashboard.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
@@ -7,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import { useQuery } from '@apollo/react-hooks';
 import { QueryMoviesById } from '../../../graphql';
 import { useStore } from '../../../Store';
+import getMovies from '../../../services';
 
 import Header1 from '../headers/Header1';
 import Table1 from '../tables/Table1';
@@ -37,13 +37,6 @@ const Dashboard: React.FC = () => {
       releaseDate: ''
     }
   });
-
-  async function getMovies() {
-    const response = await axios.get(
-      'https://api.themoviedb.org/3/search/movie?api_key=d5e0b36099a8d8c81c26b508774e8654&query=fight%20club'
-    );
-    console.log(response);
-  }
 
   function _logOut() {
     dispatch({ type: 'logout' });

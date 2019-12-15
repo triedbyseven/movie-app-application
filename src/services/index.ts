773d42cded0d4b8681653f113d5dcf93c4c0a9a8
triedbyseven <mikedev0431@gmail.com>
2019-12-15 01:16:25 -0800

    Modifed axios to use params object and added return data.
    The axios get call to the movie api was hard coding the api key and query straight into the url.
    
    Although techinically this is the desired effect we are now passing it through the built in param object inside the second arugment of axios.
    
    We are also no returning the results from teh data returned data object directly.

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,16 @@ import axios from 'axios';
 
 async function getMovies() {
   const response = await axios.get(
-    'https://api.themoviedb.org/3/search/movie?api_key=d5e0b36099a8d8c81c26b508774e8654&query=fight%20club'
+    'https://api.themoviedb.org/3/search/movie',
+    {
+      params: {
+        api_key: 'd5e0b36099a8d8c81c26b508774e8654',
+        query: 'fight club'
+      }
+    }
   );
 
-  console.log(response);
+  return response.data.results;
 }
 
 export default getMovies;

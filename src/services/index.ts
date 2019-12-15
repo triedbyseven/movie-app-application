cd9930a11d5be54884ed997c99a7402a30516d0c
triedbyseven <mikedev0431@gmail.com>
2019-12-15 14:03:25 -0800

    Modifed getMovies method with query argument.
    The getMovies method was missing the paramter `query`. We need this to pass down the query from the search query we are getting from the user but ultimately when the state changes.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-async function getMovies() {
+async function getMovies(query: string) {
   const response = await axios.get(
     'https://api.themoviedb.org/3/search/movie',
     {
       params: {
         api_key: 'd5e0b36099a8d8c81c26b508774e8654',
-        query: 'fight club'
+        query: query
       }
     }
   );
